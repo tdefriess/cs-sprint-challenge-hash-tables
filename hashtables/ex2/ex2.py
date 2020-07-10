b6255a1167eeb941982571af3a484ac264b1fb61
tdefriess <tyler.defriess@gmail.com>
2020-07-10 12:08:13 -0400

    Solved ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    route = []
+    sources = {}
+    #build dict
+    for ticket in tickets:
+        sources[ticket.source] = ticket.destination
 
+    # find starting point
+    for source in sources:
+        if sources[source] == "NONE":
+            route.append(sources["NONE"])
+
+    while len(route) < length:
+        route.append(sources[route[len(route) - 1]])
+    
     return route
